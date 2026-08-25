eee2d2f321fc77d071b733ddf9024ffcbea39a3c
aswathy <aswathysreesunil@gmail.com>
2026-08-25 20:32:33 +0530

    step 9. updated page.tsx(assembled feed page)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+import PostCard from "@/components/PostCard";
+import ComposeBox from "@/components/ComposeBox";
+import {mockPosts} from "@/lib/mock-data";
+
 export default function Home() {
-  return <div>Feed Goes here</div>;
+  return (
+    <div>
+      <header className="sticky top-0 bg-white/80 backdrop-blur-border-b border-gray-200 px-4 py-3 font-bold text-lg">
+        Home
+      </header>
+      <ComposeBox />
+      <div>
+      {mockPosts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
+  </div>
+  );
 }
+      
